cbca8002e441e0d2f4a78478b4d341e55c720d35
Sid3Script <augustraphael08@gmail.com>
2026-03-18 19:10:00 -0400

    Complete registration functionality

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,8 +1,3 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -12,8 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    # Added routes for About and Contact pages
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
