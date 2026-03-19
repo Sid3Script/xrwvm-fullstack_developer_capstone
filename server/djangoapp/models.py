e30a339f7fb49964aee44967d291bea5f4e2b58e
Raphael <175262789+Sid3Script@users.noreply.github.com>
2026-03-19 19:44:50 -0300

    Refactor year field in CarModel for readability

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -16,10 +19,13 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
+
     def __str__(self):
         return self.name
