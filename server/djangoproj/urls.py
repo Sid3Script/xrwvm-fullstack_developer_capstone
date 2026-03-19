ca73a5e9d9e6f450fd7dc1e0e793c82ccad27b41
Raphael <175262789+Sid3Script@users.noreply.github.com>
2026-03-19 19:56:07 -0300

    Add new routes for about and contact pages

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="index.html")),
+    path('about/', TemplateView.as_view(template_name="index.html")),
+    path('contact/', TemplateView.as_view(template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:id>/', TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:id>/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
