ff0ae8c75d19dbe4afa7c168edf053fd80ccce8f
Raphael <175262789+Sid3Script@users.noreply.github.com>
2026-03-19 19:58:48 -0300

    Refactor URL patterns for clarity and organization

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,22 +5,25 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # Path for registration
-    path(route='register', view=views.registration, name='register'),
-    # Path for login
     path(route='login', view=views.login_user, name='login'),
-    # Path for logout
     path(route='logout', view=views.logout_user, name='logout'),
-    # Path for get_cars
-    path(route='get_cars', view=views.get_cars, name='get_cars'),
-    # Paths for dealerships
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    # Path for dealer details
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    # Path for dealer reviews
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
-    # Path for adding a review
+    path(route='register', view=views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
     path(route='add_review', view=views.add_review, name='add_review'),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
